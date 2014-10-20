015c396527a51fb0e655ef23cc86b0d34971bbb5
leila <me@leila.cc>
2014-10-20 01:12:19 -0400

    basic push state for searching forward/back navigation

--- public/js/question_socket.js
+++ public/js/question_socket.js
@@ -1,19 +1,24 @@
 var socket = io('http://104.131.251.143:3000'),
-  results = $('#results');
+  results = $('#results'),
+  input = $("#question");
 
 socket.on("new answers", function (answers) {
   spin_div.hide();
-  for(i = 0; i < answers.length; i++) { 
-    results.append(gen_result(answers[i].evidence.title, answers[i].text));
-  }
+  pushNewQuestion('question', answers);
+  show_answers(answers);
 });
 
 socket.on("error", function(error) {
     console.log("AN ERROR OCCURRED: " + error);
 });
 
+var show_answers = function(answers) {
+  for(i = 0; i < answers.length; i++) { 
+    results.append(gen_result(answers[i].evidence.title, answers[i].text));
+  }
+}
+
 var search = function() {
-  var input = $("input[name=question]");
   var question = input[0].value;
   if (question.length == 0) {
       return;
@@ -24,6 +29,7 @@ var search = function() {
   spin_div.show();
   input.blur();
   results.empty();
+  return false;
 }
 
 var gen_result = function(title, answer) {
@@ -43,4 +49,22 @@ var gen_result = function(title, answer) {
             '</div>' + 
           '</div>'
          );
+}
+
+window.onload = function() {
+  var no_space_prev_question = $(location).attr('search').substring(1);
+  if (no_space_prev_question != '') {
+    // loading a question result page
+    var prev_question = no_space_prev_question.split('+').join(' ');
+    input[0].value = prev_question; // show prev question
+    search();
+  }
+}
+
+window.onpopstate = function() {
+  var no_space_prev_question = $(location).attr('search').substring(1);
+  show_answers(stateObj[no_space_prev_question]); // show prev answers
+
+  var prev_question = no_space_prev_question.split('+').join(' ');
+  input[0].value = prev_question; // show prev question
 }
