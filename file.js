0178616128ebcb86670050354e5f3a13f14ea1d5
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-10-28 12:26:51 -0400

    Add boilerplate for pin operations

---
+++ file.js
@@ -0,0 +1,17 @@
+var addPin(qaPair, path, userId) {
+	
+}
+
+var addFolder(name, path, userId) {
+
+}
+
+// pin could either be an id or the actual object
+var shareWithUser(canWrite, pin) {
+
+}
+
+module.exports.addPin = addPin;
+module.exports.addFolder = addFolder;
+
+// import with: file = include("./file.js")
