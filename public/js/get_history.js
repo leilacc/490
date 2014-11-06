95ffc10112314d91b66c6c8d74dbcbe793c49279
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-11-06 14:40:48 -0500

    Fix minutes not being formatted properly

--- public/js/get_history.js
+++ public/js/get_history.js
@@ -4,14 +4,19 @@ var time = "3:14 PM";
 var format_history_result = function(result) {
   var question = result.name;
   var updatedAt = new Date(result.updatedAt);
+
   var index = result.index;
 
   var scrubbed = question;
   scrubbed = scrubbed.replace('"', "&quot;").replace(' ', "+");
+  var minutes = updatedAt.getMinutes().toString();
+  if (minutes.length === 1) {
+    minutes = "0" + minutes;
+  }
 
   return ('<div class="row">' +
             '<div class="col-md-2">' +
-              '<p style="margin-left: 90px; margin-top: 18px; font-size: 20px">' + (updatedAt.getHours() % 12) + ':' + updatedAt.getMinutes() + ' PM</p>' +
+              '<p style="margin-left: 90px; margin-top: 18px; font-size: 20px">' + (updatedAt.getHours() % 12) + ':' + minutes + ' PM</p>' +
             '</div>' +
             '<div class="col-md-8">' +
               '<div class="result">' +
