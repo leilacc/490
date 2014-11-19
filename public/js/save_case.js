1554ee65a790beaeddc9213c43ee3f7a902c5a96
Leila Chan Currie <me@leila.cc>
2014-11-19 16:13:39 -0500

    removed logging statement

--- public/js/save_case.js
+++ public/js/save_case.js
@@ -1,5 +1,4 @@
 var save_case = function(case_id, folder_name) {
-  console.log('saving');
   $('#thumbtack' + case_id).attr('class', 'thumbtack-saved');
   $('#result' + case_id).append('<span class="pin-name">' + folder_name + '</span>');
 };
